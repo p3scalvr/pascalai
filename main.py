1a5353c6b8906c080fffb65b1e42ff02ba42b847
pascalvr <86066806+p3scalvr@users.noreply.github.com>
2024-11-21 16:23:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory, Response, time
-import ollama
+from flask import Flask, render_template, request, jsonify, send_from_directory, Response
+import ollama, time
 
 app = Flask(__name__, template_folder='templates')
 
